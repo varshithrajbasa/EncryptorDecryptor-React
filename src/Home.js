f5cc284a1bc91392fe20e0c51da2aae7be0ccd22
Basa Varshith Raj <varshithraj.basa@gmail.com>
2021-08-15 12:45:41 +0530

    Added Home Page

---
+++ src/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return(
+        <div>
+            <h1>Welcome to Converter App</h1>
+            <h2>Encrypt or Decrypt text</h2>
+        </div>
+    );
+}
+export default Home;
